29a8a0b6a61ea93c6c4a0e557482cb13447153cf
Shutko Illia <illasutko@gmail.com>
2025-01-26 16:15:57 +0200

    The finished site for notebooks

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// const header = document.querySelector('header');
+
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY > 200 && !header.classList.contains('header--shrink')) { 
+//         header.classList.remove('header--common-height'); 
+//         header.classList.add('header--shrink'); 
+//     } 
+//     if (window.scrollY < 100 && !header.classList.contains('header--common-height')) { 
+//         header.classList.remove('header--shrink');
+//         header.classList.add('header--common-height'); 
+
+//     } 
+// });
