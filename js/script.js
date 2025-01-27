8a0eafadaea1c15baa69dfa86dddbea6444c3b32
Shutko Illia <illasutko@gmail.com>
2025-01-27 14:43:49 +0200

    File managing, edits of adaptation

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-// const header = document.querySelector('header');
+const header = document.querySelector('header');
 
-// window.addEventListener('scroll', () => {
-//     if (window.scrollY > 200 && !header.classList.contains('header--shrink')) { 
-//         header.classList.remove('header--common-height'); 
-//         header.classList.add('header--shrink'); 
-//     } 
-//     if (window.scrollY < 100 && !header.classList.contains('header--common-height')) { 
-//         header.classList.remove('header--shrink');
-//         header.classList.add('header--common-height'); 
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200 && !header.classList.contains('header--shrink')) { 
+        header.classList.remove('header--common-height'); 
+        header.classList.add('header--shrink'); 
+    } 
+    if (window.scrollY < 100 && !header.classList.contains('header--common-height')) { 
+        header.classList.remove('header--shrink');
+        header.classList.add('header--common-height'); 
 
-//     } 
-// });
+    } 
+});
